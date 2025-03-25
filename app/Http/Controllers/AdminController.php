d32e8c46f3568a79ea3845be1ea2b37cde483a09
BeateSannija <beate.aploka@gmail.com>
2025-03-25 23:37:12 +0200

    Removed outdated comments

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,14 +12,6 @@ class AdminController extends Controller
 {
     public function index()
     {
-        // return view('admin.index');
-        // Fetch orders with status 'accepted' and progress 'null' or 'in_progress'
-        /*$orders = Order::with(['user', 'cartItems.dish'])
-                ->where('status', 'accepted')
-                ->whereIn('progress', [null, 'in_progress'])
-                ->get();
-
-        return view('admin.index', compact('orders'));*/
         $orders = Order::with(['user', 'cartItems.dish'])
             ->where('status', 'accepted')
             ->where(function ($query) { $query->where('progress', 'in_progress')->orWhereNull('progress');})->get();
@@ -30,18 +22,6 @@ class AdminController extends Controller
 
     public function update_order_progress(Request $request, $id)
     {
-        /*$order = Order::find($id);
-
-        if ($order) {
-            $order->progress = $request->input('progress');
-            $order->save();
-
-            return redirect()->back()->with('success', 'Order progress updated successfully!');
-        }
-
-        return redirect()->back()->with('error', 'Order not found!');*/
-
-
         $order = Order::findOrFail($id);
         $order->progress = $request->input('progress');
         $order->save();
@@ -58,10 +38,6 @@ class AdminController extends Controller
 
     public function view_requests()     //needs to show orders with no statuss yet
     {
-        //$orders = Order::all();
-        /*$orders = Order::with(['user', 'cartItems.dish'])->get();
-        return view('admin.requests', compact('orders'));*/
-
         $orders = Order::with('cartItems.dish')->get();
         return view('admin.requests', compact('orders'));
     }
@@ -92,27 +68,6 @@ class AdminController extends Controller
         return view('admin.addDish');
 
     }
-
-    // for 'Dish' Model
-    /*public function add_dish(Request $request)
-    {
-        $dish = new Dish;
-        $dish->dish_name = $request->input('dish-name');
-        $dish->dish_description = $request->input('dish-description');
-        $dish->dish_price = $request->input('dish-price');
-        $dish->dish_category = $request->input('dish-category');
-
-        if ($request->hasFile('image') && $request->file('image')->isValid()) 
-        {
-            $image = $request->file('image');
-            $imagename = time() . '.' . $image->getClientOriginalExtension();
-            $path = $image->storeAs('dishes', $imagename, 'public');
-            $dish->image = $path;
-        }
-        $dish->save();
-        return redirect()->route('admin.view_dishes');
-    }*/
-
     //updated add_dish method where also english translation is added
     public function add_dish(Request $request)
     {
@@ -137,9 +92,6 @@ class AdminController extends Controller
         return redirect()->route('admin.view_dishes');
     }
     
-
-
-
     public function delete_dish($id)
     {
         $dish = Dish::find($id);
@@ -195,9 +147,6 @@ class AdminController extends Controller
         ]);
 
         $dish = Dish::find($id);
-        /*if (!$dish) {
-            return redirect()->back()->with('error', 'Dish not found!');
-        }*/
 
         // Update the dish status
         $dish->status = $request->input('dish-status');
